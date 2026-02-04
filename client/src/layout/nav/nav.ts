a863382a2c0e9268eb2509504ee44939989eabb0
Your Name <you@example.com>
2026-02-04 11:07:03 +0200

    Sec 6

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,31 +1,36 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private taost = inject(ToastService);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (result) => {
-        console.log(result);
-        this.creds = {
-          email: '',
-          password: '',
-        };
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.taost.success('Logged In Successfully');
+        this.creds = {};
       },
-      error: (error) => alert(error.message),
+      error: (error) => {
+        this.taost.error(error.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
